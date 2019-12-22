ee841c1f4a501168e06cb7914fb648a2f1feea7e
MalihaShaheed <malihashaheed@MalihaShaheeds-MacBook-Air.local>
2019-12-22 11:01:45 -0500

    updates

--- src/middleware/cors-middleware.ts
+++ src/middleware/cors-middleware.ts
@@ -1,11 +1,12 @@
-import { Request } from 'express'
+import { Request } from 'express';
 
-export function corsFilter(req: Request, res, next){
+export function corsFilter(req: Request, res, next) {
     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`);
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, token');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    //headers.putSingle("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Expose-Headers', 'token')
-    if (req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.status(200).send();
     } else {
         next();
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,10 +1,28 @@
+
 import { Pool } from 'pg';
 
+require('dotenv').config();
+//const pg = require('pg');
+const {ERS_USERNAME, ERS_HOST} = process.env;
+
+console.log(`Connection to ${ERS_USERNAME} @ ${ERS_HOST}`);
+
+// export const connectionPool: Pool = new Pool({
+//     user: process.env['ERS_USERNAME'],
+//     host: process.env['ERS_HOST'],
+//     database: process.env['ERS_DATABASE'],
+//     password: process.env['ERS_PASSWORD'],
+//     port: 5432,
+//     max: 5,
+// });     
+
 export const connectionPool: Pool = new Pool({
-    user: process.env['ERS_USERNAME'],
-    host: process.env['ERS_HOST'],
-    database: process.env['ERS_DATABASE'],
-    password: process.env['ERS_PASSWORD'],
-    port: 5432,
-    max: 5,
-});
+        user: "ms46776",
+        host: "ms46776.cvk2ng2gqd8h.us-east-1.rds.amazonaws.com",
+        database: "postgres",
+        password: "Fordhamuni19!",
+        port: 5432,
+        max: 5,
+    });  
+
+//module.exports = new pg.Pool();
